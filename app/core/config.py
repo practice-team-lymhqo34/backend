14169c38ca9264cbb84d1415e8ec1ed2291d8320
Dima Horinyuk <dima.horinyuk27@gmail.com>
2026-04-20 21:57:43 +0300

    chore: PT-79 update dependencies and config for authentication

--- app/core/config.py
+++ app/core/config.py
@@ -1,20 +1,15 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "logiflow"
-
-    # Додай цей рядок
-    DATABASE_URL: str = "sqlite:///./sql_app.db"
-
-    SECRET_KEY: str = "temporary_secret_for_dev_only"
+    DATABASE_URL: str
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     COOKIE_SECURE: bool = False
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
